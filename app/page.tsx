0687c6c354141416b0b648168f01ef38e1b3ec1b
Nimira43 <n.rabone72@aol.com>
2025-09-16 00:08:50 +0100

    Logo font changed. Logo text restyled. Custom colours brought into tailwind-config. Home page cleaned up of logo image markup.

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,13 @@
 import Image from 'next/image'
-// import Logo from './assets/logo.png'
 import LandingImg from './assets/hero-image.jpg'
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
 export default function Home() {
   return (
     <main>
-      {/* <header className='max-w-6xl mx-auto px-4 sm:px-8 py-6 '>
-        <Image src={Logo} alt='logo' />
-      </header> */}
       <section className='max-w-6xl mx-auto px-4 sm:px-8 h-screen -mt-20 grid lg:grid-cols-[1fr,400px] items-center'>
         <div>
-          <h1 className='logo text-4xl font-light md:text-7xl font-medium'>
+          <h1 className='logo text-5xl text-main font-bold tracking-wide md:text-7xl font-medium'>
             Ergon
           </h1>
           <p className='leading-loose max-w-md mt-4 '>
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,15 @@ module.exports = {
   			sm: 'calc(var(--radius) - 4px)'
   		},
   		colors: {
-  			background: 'hsl(var(--background))',
+				'main': '#ff4500',
+        'support': '#87ceeb',
+				'dark': '#111',
+        'light': '#fffcfa',
+        'grey-dark': '#333',
+  			'grey-medium': '#999',
+				'grey-light': '#ccc',
+				'grey-extra-light': '#eee',
+				background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
   			card: {
   				DEFAULT: 'hsl(var(--card))',
