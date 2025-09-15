865cb78d84c43397342533306842441d56d0dc05
Nimira43 <n.rabone72@aol.com>
2025-09-15 23:45:51 +0100

    Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,28 @@
-const HomePage = () => {
-  return ( 
-    <div className='p-6 bg-amber-400'>
-      <h1 className='text-4xl text-white'>Ergon</h1>
-    </div>
-   )
+import Image from 'next/image'
+import Logo from './assets/logo.png'
+import LandingImg from './assets/user-5.jpg'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+export default function Home() {
+  return (
+    <main>
+      {/* <header className='max-w-6xl mx-auto px-4 sm:px-8 py-6 '>
+        <Image src={Logo} alt='logo' />
+      </header> */}
+      <section className='max-w-6xl mx-auto px-4 sm:px-8 h-screen -mt-20 grid lg:grid-cols-[1fr,400px] items-center'>
+        <div>
+          <h1 className='capitalize text-4xl md:text-7xl font-bold'>
+            Ergon
+          </h1>
+          <p className='leading-loose max-w-md mt-4 '>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ab ex assumenda quas excepturi, quam consequuntur, debitis dolores quia sed nemo, similique dolore libero rerum animi dolorum repellendus corrupti natus? Voluptate laborum corporis soluta veniam vel quibusdam ipsum vitae iure?
+          </p>
+          <Button asChild className='mt-4'>
+            <Link href='/add-job'>Get Started</Link>
+          </Button>
+        </div>
+        <Image src={LandingImg} alt='landing' className='hidden lg:block ' />
+      </section>
+    </main>
+  )
 }
- 
-export default HomePage
