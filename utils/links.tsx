bcffc5abffde85c4f2983976bbec776034762222
Nimira43 <n.rabone72@aol.com>
2025-09-17 21:12:44 +0100

    Created NavLink with type NavLink

--- utils/links.tsx
+++ utils/links.tsx
@@ -1,3 +1,29 @@
 import { AiOutlineAreaChart } from 'react-icons/ai'
 import { IoLayersOutline } from 'react-icons/io5'
 import { PiAppWindow } from 'react-icons/pi'
+
+type NavLink = {
+  href: string
+  label: string
+  icon: React.ReactNode
+}
+
+const links: NavLink[] = [
+  {
+    href: '/add-job',
+    label: 'add job',
+    icon: <IoLayersOutline />,
+  },
+  {
+    href: '/jobs',
+    label: 'all jobs',
+    icon: <PiAppWindow />,
+  },
+  {
+    href: '/stats',
+    label: 'stats',
+    icon: <AiOutlineAreaChart />,
+  },
+]
+
+export default links
