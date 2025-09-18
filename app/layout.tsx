17de771c054d76db4703b9f8fc73baae7e58b35b
Nimira43 <n.rabone72@aol.com>
2025-09-18 20:55:17 +0100

    Wrapped children in Providers in root layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
+import Providers from './providers'
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -22,7 +23,9 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang='en'>
         <body className={poppins.className}>
-          {children}
+          <Providers>
+            {children}
+          </Providers>
         </body>
       </html>
     </ClerkProvider>
