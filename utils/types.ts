fb63cc42e05cdce4271875f132a98dcd1dbbd021
Nimira43 <n.rabone72@aol.com>
2025-09-29 23:51:21 +0100

    Created JobType

--- utils/types.ts
+++ utils/types.ts
@@ -1,1 +1,13 @@
 import * as z from 'zod'
+
+export type JobType = {
+  id: string
+  createdAt: Date
+  updatedAt: Date
+  clerkId: string
+  position: string
+  company: string
+  location: string
+  status: string
+  mode: string
+}
