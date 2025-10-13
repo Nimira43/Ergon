0f602b71a14453e8cb8a5dcd11497b855fcadc8f
Nimira43 <n.rabone72@aol.com>
2025-10-13 22:04:41 +0100

    Removed unwanted imports and old code

--- components/CreateJobForm.tsx
+++ components/CreateJobForm.tsx
@@ -2,18 +2,10 @@
 
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
-import * as z from 'zod'
 import { Button } from '@/components/ui/button'
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
-import { Input } from '@/components/ui/input'
+import { Form } from '@/components/ui/form'
 import { JobStatus, JobMode, createAndEditJobSchema, CreateAndEditJobType } from '@/utils/types'
 import { CustomFormField, CustomFormSelect } from './FormComponents'
-
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: 'Username must be at least 2 characters.',
-  }),
-})
 
 function CreateJobForm() {
   const form = useForm<CreateAndEditJobType>({
