7e6707fb7cee1929d983503a72f8a9726a481fe0
Nimira43 <n.rabone72@aol.com>
2025-10-13 21:48:37 +0100

    import JobStatus, JobMode, createAndEditJobSchema, CreateAndEditJobType into CreateJobForm

--- components/CreateJobForm.tsx
+++ components/CreateJobForm.tsx
@@ -6,6 +6,7 @@ import * as z from 'zod'
 import { Button } from '@/components/ui/button'
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
+import { JobStatus, JobMode, createAndEditJobSchema, CreateAndEditJobType } from '@/utils/types'
 
 const formSchema = z.object({
   username: z.string().min(2, {
